15d41581de510dc2b62d3d1bdf370cd4d807e3e5
kangwook <rlarkddnr1686@naver.com>
2024-03-21 21:09:04 +0900

    구간합구하기 - StringTokenizer과 BufferedReader 사용

--- src/main/java/com/Algorithm/doitcoding/datastructure/구간합구하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/구간합구하기.java
@@ -1,33 +1,32 @@
 package com.Algorithm.doitcoding.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 구간합구하기 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int questionSize = sc.nextInt();
-        int[] numbers = new int[size];
-        int[] sums = new int[size];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        for (int i=0;i<size;i++) {
-            numbers[i] = sc.nextInt();
+        int size = Integer.parseInt(stringTokenizer.nextToken());
+        int questionSize = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[size + 1];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i=1;i<=size;i++) {
+            S[i] = S[i-1] +Integer.parseInt(stringTokenizer.nextToken());
         }
 
-        sums[0] = numbers[0];
-
-        for (int i=1;i<size;i++) {
-            sums[i] = sums[i - 1] + numbers[i];
-        }
 
         for (int i=0;i<questionSize;i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            if (start == 1) {
-                System.out.println(sums[end-1]);
-            } else {
-                System.out.println(sums[end-1]-sums[start-2]);
-            }
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(stringTokenizer.nextToken());
+            int end = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[end]-S[start-1]);
         }
     }
 }
