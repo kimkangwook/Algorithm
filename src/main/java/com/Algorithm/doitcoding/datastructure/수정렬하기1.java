107de6f7c2eb86159748096818f3844b67bf29a8
kangwook <rlarkddnr1686@naver.com>
2024-08-07 20:50:31 +0900

    수정렬하기 1번 : 버블정렬 풀이

--- src/main/java/com/Algorithm/doitcoding/datastructure/수정렬하기1.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/수정렬하기1.java
@@ -1,9 +1,41 @@
 package com.Algorithm.doitcoding.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class 수정렬하기1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
+        int[] arr = new int[i];
 
+        for (int j=0;j<i;j++) {
+            arr[j] = Integer.parseInt(br.readLine());
+        }
+
+        // 버블 정렬
+        // 라운드 수는 배열의 size-1
+        for (int k=1;k<i;k++) {
+
+            for (int l=0;l<i-k;l++) {
+                if (arr[l]>arr[l+1]) {
+                    int temp = arr[l];
+                    arr[l] = arr[l+1];
+                    arr[l+1] = temp;
+                }
+
+            }
+
+        }
+
+        for (int m=0;m<i;m++) {
+            System.out.println(arr[m]);
+        }
+
+
+    }
 
 
 }
