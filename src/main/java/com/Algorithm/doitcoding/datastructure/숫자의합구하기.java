b5ddc1c833bd4f5fb9b4616fb3a76451b3e188db
kangwook <rlarkddnr1686@naver.com>
2024-08-19 21:34:37 +0900

    숫자의합구하기 : 배열 사용 풀이

--- src/main/java/com/Algorithm/doitcoding/datastructure/숫자의합구하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/숫자의합구하기.java
@@ -1,21 +1,25 @@
 package com.Algorithm.doitcoding.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class 숫자의합구하기 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int numberSize = sc.nextInt();
-        // 입력값을 String형 변수 sNum에 저장한 후 char[]형 변수로 변환하기
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
-        int sum = 0;
-
-        for (int i=0;i<numberSize;i++) {
-            sum += cNum[i]-'0'; // cNum[i]를 정수형으로 변환하면서 sum에 더하여 누적하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String arr = "";
+        for (int i=0;i<N;i++) {
+            arr += Integer.valueOf(br.read()-'0');
         }
 
-        System.out.println("나머지 합계 : " +sum);
+        char[] chars = arr.toCharArray();
+        int sum=0;
+        for (int i=0;i<chars.length;i++) {
+            sum+=chars[i] - '0';
+        }
+        System.out.println(sum);
     }
 }
