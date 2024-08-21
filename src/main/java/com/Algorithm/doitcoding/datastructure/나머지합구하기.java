1ae372cb69839cd2d970a8b62889cf09f317a82b
kangwook <rlarkddnr1686@naver.com>
2024-08-21 23:19:10 +0900

    나머지 합 구하기 : int -> long 배열로 변환

--- src/main/java/com/Algorithm/doitcoding/datastructure/나머지합구하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/나머지합구하기.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class 나머지합구하기 {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
