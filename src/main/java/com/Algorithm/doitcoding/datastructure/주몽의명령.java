53fa5234fbe638ebed61c6f6309ac9709cfbd68c
kangwook <rlarkddnr1686@naver.com>
2024-08-26 22:55:34 +0900

    투 포인터

--- src/main/java/com/Algorithm/doitcoding/datastructure/주몽의명령.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/주몽의명령.java
@@ -10,32 +10,33 @@ import java.util.StringTokenizer;
 public class 주몽의명령 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        Integer N = Integer.parseInt(br.readLine());
+        Integer M = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
+        int[] arr = new int[N];
+
         for (int i=0;i<N;i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(A);
+        Arrays.sort(arr);
+        int start_index = 0;
+        int end_index = N - 1;
+        int count = 0;
 
-        int i=0;
-        int j=N-1;
-        int count=0;
-        while(i<j) {
-            if (A[i]+A[j]>M) {
-                j--;
-            } else if (A[i]+A[j]<M) {
-                i++;
-            } else if (A[i]+A[j]==M) {
+        while (start_index!=end_index && start_index<end_index) {
+            if (arr[start_index] + arr[end_index] > M) {
+                end_index--;
+            } else if (arr[start_index] + arr[end_index] < M) {
+                start_index++;
+            } else {
                 count++;
-                i++;
-                j--;
+                start_index++;
+                end_index--;
             }
         }
+
         System.out.println(count);
-        br.close();
     }
 }
