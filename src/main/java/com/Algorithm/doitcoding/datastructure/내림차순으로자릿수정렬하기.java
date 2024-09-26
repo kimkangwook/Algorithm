f67689ddc404a18f684902242ae68022b23cf34d
kangwook <rlarkddnr1686@naver.com>
2024-09-26 22:05:48 +0900

    내림차순으로 자릿수 정렬하기

--- src/main/java/com/Algorithm/doitcoding/datastructure/내림차순으로자릿수정렬하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/내림차순으로자릿수정렬하기.java
@@ -3,38 +3,17 @@ package com.Algorithm.doitcoding.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class 내림차순으로자릿수정렬하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] chars = br.readLine().toCharArray();
-        int[] arr = new int[chars.length];
-        for (int i=0;i<chars.length;i++) {
-            arr[i] = chars[i] - '0';
-        }
 
-        for (int i=0;i<chars.length-1;i++) {
+        Arrays.sort(chars);
 
-            int max = 0;
-            int index = 0;
-            for (int j=i;j<chars.length;j++) {
-                if (max<arr[j]) {
-                    max = arr[j];
-                    index = j;
-                }
-            }
-
-            if (i==0||index!=0) {
-                int temp = arr[index];
-                arr[index] = arr[i];
-                arr[i] = temp;
-            }
-
-
-        }
-
-        for (int i=0; i<arr.length;i++) {
-            System.out.print(arr[i]);
+        for (int i=chars.length-1;i>=0;i--) {
+            System.out.print(chars[i]);
         }
 
     }
