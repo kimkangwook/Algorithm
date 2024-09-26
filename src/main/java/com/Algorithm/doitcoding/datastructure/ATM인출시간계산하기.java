b15054c0d60d5485a88ae7e4a1f68b121cba939a
kangwook <rlarkddnr1686@naver.com>
2024-09-26 23:01:36 +0900

    ATM인출시간계산하기(Array 정렬 이용)

--- src/main/java/com/Algorithm/doitcoding/datastructure/ATM인출시간계산하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/ATM인출시간계산하기.java
@@ -3,6 +3,7 @@ package com.Algorithm.doitcoding.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ATM인출시간계산하기 {
@@ -10,40 +11,20 @@ public class ATM인출시간계산하기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
-        int[] S = new int[N];
-
+        int[] arr = new int[N];
+        int[] sum = new int[N];
         for (int i=0;i<N;i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(arr);
+        sum[0] = arr[0];
+        int sumArray = 0;
         for (int i=1;i<N;i++) {
-            int insert_point = i;
-            int insert_value = A[i];
-
-            for (int j=i-1;j>=0;j--) {
-                if (A[i] > A[j]) {
-                    insert_point = j+1;
-                    break;
-                }
-                if (j==0) {
-                    insert_point = 0;
-                }
-            }
-            for (int j=i;j>insert_point;j--) {
-                A[j] = A[j - 1];
-            }
-            A[insert_point] = insert_value;
+            sum[i] = sum[i - 1] + arr[i];
+            sumArray += sum[i];
         }
 
-        S[0] = A[0];
-        for (int i=1;i<N;i++) {
-            S[i] = S[i - 1] + A[i];
-        }
-        int sum=0;
-        for (int i=0;i<N;i++) {
-            sum += S[i];
-        }
-        System.out.println(sum);
+        System.out.println(sumArray+sum[0]);
     }
 }
