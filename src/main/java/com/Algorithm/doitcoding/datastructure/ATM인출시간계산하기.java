2b74e088001715466153d4a0ba38f4dd645aa19d
kangwook <rlarkddnr1686@naver.com>
2024-09-29 12:54:37 +0900

    ATM 삽입정렬 구현

--- src/main/java/com/Algorithm/doitcoding/datastructure/ATM인출시간계산하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/ATM인출시간계산하기.java
@@ -3,28 +3,56 @@ package com.Algorithm.doitcoding.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ATM인출시간계산하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] arr = new int[N];
         int[] sum = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i=0;i<N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
-        sum[0] = arr[0];
-        int sumArray = 0;
         for (int i=1;i<N;i++) {
-            sum[i] = sum[i - 1] + arr[i];
-            sumArray += sum[i];
+            int insert_point = i;
+            int insert_value = arr[i];
+
+            for (int j=i-1;j>=0;j--) {
+                if (arr[j] < arr[i]) {
+                    insert_point = j + 1;
+                    break;
+                }
+                if (j==0) {
+                    insert_point = 0;
+                }
+            }
+
+            for (int j=i-1;j>=insert_point;j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[insert_point] = insert_value;
         }
 
-        System.out.println(sumArray+sum[0]);
+        sum[0] = arr[0];
+        for (int i=1;i<arr.length;i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        int S = 0;
+
+        for (int i=0;i<sum.length;i++) {
+            S += sum[i];
+        }
+        System.out.println(S);
+
+
+
+
     }
 }
