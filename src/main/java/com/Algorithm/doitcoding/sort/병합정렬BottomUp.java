e654d71b720ed8a5ecc1cf89ab1156d25b85a9f1
kangwook <rlarkddnr1686@naver.com>
2024-11-11 22:35:23 +0900

    병합정렬

--- src/main/java/com/Algorithm/doitcoding/sort/병합정렬BottomUp.java
+++ src/main/java/com/Algorithm/doitcoding/sort/병합정렬BottomUp.java
@@ -4,62 +4,94 @@ public class 병합정렬BottomUp {
 
     private static int[] sorted;
 
-    public static void 병합정렬(int[] a) {
+    public static void merge_sort(int[] a) {
         sorted = new int[a.length];
-        병합정렬(a, 0, a.length - 1);
-        sorted=null;
+        merge_sort(a, 0, a.length - 1);
+        sorted = null;
     }
 
-    private static void 병합정렬(int[] a, int left, int right) {
+    // Bottom-Up 방식 구현
+    private static void merge_sort(int[] a, int left, int right) {
+        /*
+         * 1 - 2 - 4 - 8 - ... 식으로 1부터 서브리스트를 나누는 기준을 두 배씩 늘린다.
+         */
+        for (int size = 1; size <= right; size += size) {
 
-        for (int size=1; size <= right; size += size) {
-
-            for (int l=0;l<=right-size;l += (2*size)) {
+            /*
+             * 두 부분리스트를 순서대로 병합해줌
+             * 예를 들면 현재 부분리스트의 크기가 1(size=1)일 때
+             * 왼쪽 부분리스트(low ~ mid)와 오른쪽 부분리스트(mid+1 ~ high)를 생각하면
+             * 왼쪽 부분리스트는 low = mid = 0 이고,
+             * 오른쪽 부분리스트는 mid + 1부터 low + (2 * size) - 1 = 1됨
+             * 이 때 high가 배열의 인덱스를 넘어갈 수 있으므로 right와 둘 중 작은 값이 병합되도록 함
+             */
+            for (int l=0; l <= right - size; l += (2 * size)) {
                 int low = l;
                 int mid = l + size - 1;
                 int high = Math.min(l + (2 * size) - 1, right);
-                병합(a, low, mid, high);
+                merge(a, low, mid, high);
             }
         }
-
     }
 
-    private static void 병합(int[] a, int left, int mid, int right) {
-        int l = left;
-        int r = mid + 1;
-        int idx = left;
+    private static void merge(int[] a, int left, int mid, int right) {
+        int l = left;		// 왼쪽 부분리스트 시작점
+        int r = mid + 1;	// 오른쪽 부분리스트의 시작점
+        int idx = left;		// 채워넣을 배열의 인덱스
 
-        while (l<=mid && r<=right) {
-            if (a[l]<=a[r]) {
+
+        while(l <= mid && r <= right) {
+            /*
+             *  왼쪽 부분리스트 l번째 원소가 오른쪽 부분리스트 r번째 원소보다 작거나 같을 경우
+             *  왼쪽의 l번째 원소를 새 배열에 넣고 l과 idx를 1 증가시킨다.
+             */
+            if(a[l] <= a[r]) {
                 sorted[idx] = a[l];
                 idx++;
                 l++;
-            } else {
+            }
+            /*
+             *  오른쪽 부분리스트 r번째 원소가 왼쪽 부분리스트 l번째 원소보다 작거나 같을 경우
+             *  오른쪽의 r번째 원소를 새 배열에 넣고 r과 idx를 1 증가시킨다.
+             */
+            else {
                 sorted[idx] = a[r];
                 idx++;
                 r++;
             }
         }
 
-        if (r>right) {
-            while (l<=mid) {
-                sorted[idx] = a[l];
-                idx++;
-                l++;
-            }
-        } else {
-            while (r<=right) {
+        /*
+         * 왼쪽 부분리스트가 먼저 모두 새 배열에 채워졌을 경우 (l > mid)
+         * = 오른쪽 부분리스트 원소가 아직 남아있을 경우
+         * 오른쪽 부분리스트의 나머지 원소들을 새 배열에 채워준다.
+         */
+        if(l > mid) {
+            while(r <= right) {
                 sorted[idx] = a[r];
                 idx++;
                 r++;
             }
         }
 
-        for (int i=left;i<=right;i++) {
+        /*
+         * 오른쪽 부분리스트가 먼저 모두 새 배열에 채워졌을 경우 (r > right)
+         * = 왼쪽 부분리스트 원소가 아직 남아있을 경우
+         * 왼쪽 부분리스트의 나머지 원소들을 새 배열에 채워준다.
+         */
+        else {
+            while(l <= mid) {
+                sorted[idx] = a[l];
+                idx++;
+                l++;
+            }
+        }
+
+        /*
+         * 정렬된 새 배열을 기존의 배열에 복사하여 옮겨준다.
+         */
+        for(int i = left; i <= right; i++) {
             a[i] = sorted[i];
         }
-
-
-
     }
 }
--- src/main/java/com/Algorithm/doitcoding/sort/병합정렬TopDown.java
+++ src/main/java/com/Algorithm/doitcoding/sort/병합정렬TopDown.java
@@ -2,46 +2,57 @@ package com.Algorithm.doitcoding.sort;
 
 public class 병합정렬TopDown {
 
-    private static int[] sorted;
+    private static int[] sorted; // 합치는 과정에서 정렬하여 원소를 담을 임시 배열
 
-    public static void 병합정렬(int[] a) {
+    public static void merge_sort(int[] a) {
         sorted = new int[a.length];
-        병합정렬(a, 0, a.length - 1);
-        sorted = null;
+        merge_sort(a, 0, a.length - 1);
+        sorted  = null;
     }
 
-    // Top-Down 방식 구현
-    private static void 병합정렬(int[] a, int left, int right) {
+    private static void merge_sort(int[] a, int left, int right) {
 
-        // left==right는 부분리스트가 1개의 원소만 갖는 경우
-        if (left==right) return;
+        if (left == right) return;
 
-        int mid = (left + right) / 2; // 절반 위치
+        int mid = (left + right) / 2;  // 절반 위치
 
-        병합정렬(a, left, mid);
-        병합정렬(a, mid + 1, right);
+        merge_sort(a,left,mid); // 절반 중 왼쪽 부분리스트(left ~ mid)
+        merge_sort(a, mid + 1, right); // 절반 중 오른쪽 부분리스트(mid+1 ~ right)
 
-        병합(a, left, mid, right);
+        merge(a, left, mid, right); // 병합 작업
     }
 
-    private static void 병합(int[] a, int left, int mid, int right) {
-        int l = left;
-        int r = mid+1;
-        int idx = left;
+    private static void merge(int[] a, int left, int mid, int right) {
+        int l = left; // 왼쪽 부분리스트 시작점
+        int r = mid + 1; // 오른쪽 부분리스트 시작점
+        int idx = left; // 채워넣을 배열의 인덱스
 
         while (l<=mid && r<=right) {
+            /*
+             * 왼쪽 부분리스트 l번째 원소가 오른쪽 부분리스트 r번째 원소보다 작거나 같을 경우
+             * 왼쪽의 l번째 원소를 새 배열에 넣고 l과 idx를 1 증가시킴
+             */
             if (a[l]<=a[r]) {
                 sorted[idx] = a[l];
+                idx++;
                 l++;
-                idx++;
-            } else {
+            }
+            /*
+             * 오른쪽 부분리스트 r번째 원소가 왼쪽 부분리스트 l번째 원소보다 작을 경우
+             * 오른쪽 r번째 원소를 새 배열에 넣고 r과 idx를 1 증가시킴
+             */
+            else {
                 sorted[idx] = a[r];
-                r++;
                 idx++;
+                r++;
             }
         }
 
-        if (l>mid) {
+        /*
+         * 왼쪽 부분리스트가 먼저 모두 새 배열에 채워졌을 경우 (l > mid)
+         * 오른쪽 부분리스트의 나머지 원소들을 새 배열에 채워줌 (반대로도 마찬가지)
+         */
+        if (l > mid) {
             while (r<=right) {
                 sorted[idx] = a[r];
                 idx++;
@@ -55,8 +66,12 @@ public class 병합정렬TopDown {
             }
         }
 
+        /*
+         * 정렬된 새 배열을 기존의 배열에 복사하여 옮겨줌
+         */
         for (int i=left;i<=right;i++) {
             a[i] = sorted[i];
         }
     }
+
 }
