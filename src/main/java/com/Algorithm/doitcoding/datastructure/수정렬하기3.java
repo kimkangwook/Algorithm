c665b4d615a56816ba777d81b27c19b5f058fec5
kangwook <rlarkddnr1686@naver.com>
2024-12-04 23:19:07 +0900

    기수정렬사용

--- src/main/java/com/Algorithm/doitcoding/datastructure/수정렬하기3.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/수정렬하기3.java
@@ -4,7 +4,6 @@ package com.Algorithm.doitcoding.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class 수정렬하기3 {
 
@@ -13,56 +12,55 @@ public class 수정렬하기3 {
         int N = Integer.parseInt(br.readLine());
         int[] array = new int[N];
         int max = 0;
-
-        // 입력받으며 최대값 계산
-        for (int i = 0; i < N; i++) {
+        for (int i=0;i<N;i++) {
             array[i] = Integer.parseInt(br.readLine());
-            if (array[i] > max) max = array[i];
+            if (max < array[i]) max = array[i];
         }
 
-        // 기수 정렬 실행
         radixSort(array, max);
 
-        // 정렬된 결과 출력
         StringBuilder sb = new StringBuilder();
-        for (int num : array) {
-            sb.append(num).append('\n');
+        for (int i=0;i<N;i++) {
+            sb.append(array[i]).append('\n');
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    private static void radixSort(int[] array, int max) {
+    public static void radixSort(int[] array, int max) {
         int n = array.length;
-        int[] output = new int[n]; // 정렬 결과를 임시로 저장할 배열
-        int digit = 1;            // 현재 처리할 자릿수 (1, 10, 100, ...)
-        int[] count = new int[10]; // 자릿수별 카운트 배열
+        int[] count = new int[10];
+        int[] output = new int[n];
+        int digit = 1;
 
-        // 최대 자릿수까지 반복
-        while (max / digit > 0) {
-            // 카운트 배열 초기화
-            for (int i = 0; i < 10; i++) count[i] = 0;
+        while (max/digit>0) {
 
-            // 자릿수별 등장 횟수 계산
-            for (int i = 0; i < n; i++) {
+            // 카운트 배열 정리
+            for (int i=0;i<10;i++) {
+                count[i] = 0;
+            }
+
+            // 기존 배열 돌면서 카운트 배열 채우기
+            for (int i=0;i<n;i++) {
                 int index = (array[i] / digit) % 10;
                 count[index]++;
             }
 
-            // 누적 합으로 위치 계산
-            for (int i = 1; i < 10; i++) {
+            // 카운트 배열 누적합으로 위치 정하기
+            for (int i=1;i<10;i++) {
                 count[i] += count[i - 1];
             }
 
-            // 결과를 임시 배열에 저장
-            for (int i = n - 1; i >= 0; i--) {
+            // output 배열에 순서대로 담기
+            // 누적합이기 때문에 각 자릿수의 큐에서 맨뒤부터 차례로 담아줌
+            for (int i=n-1;i>=0;i--) {
                 int index = (array[i] / digit) % 10;
-                output[--count[index]] = array[i];
+                output[--count[index]] =array[i];
             }
 
-            // 정렬된 결과를 원본 배열에 복사
+            // 원본 배열에 옮겨주기
             System.arraycopy(output, 0, array, 0, n);
 
-            // 다음 자릿수로 이동
+            // 자릿수 올리기
             digit *= 10;
         }
     }
