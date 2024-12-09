7af586599eff78a184b844700a753581784657cd
kangwook <rlarkddnr1686@naver.com>
2024-12-10 00:04:12 +0900

    연결 요소의 개수 구하기(DFS)

--- src/main/java/com/Algorithm/doitcoding/datastructure/연결요소의개수구하기.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/연결요소의개수구하기.java
@@ -8,50 +8,59 @@ import java.util.StringTokenizer;
 
 public class 연결요소의개수구하기 {
 
-    static ArrayList<Integer>[] A;
-    static boolean[] visited;
+    public static ArrayList<Integer>[] arrays;
+
+    public static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1];
-        A = new ArrayList[N + 1];
+
+        visited = new boolean[N+1];
+        arrays = new ArrayList[N + 1];
 
         for (int i=1;i<N+1;i++) {
-            A[i] = new ArrayList<Integer>();
+            arrays[i] = new ArrayList<Integer>();
         }
 
         for (int i=0;i<M;i++) {
-
             st = new StringTokenizer(br.readLine());
 
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            A[u].add(v);
-            A[v].add(u);
+            arrays[S].add(E);
+            arrays[E].add(S);
         }
 
         int count = 0;
         for (int i=1;i<N+1;i++) {
+
             if (!visited[i]) {
                 count++;
                 DFS(i);
             }
         }
         System.out.println(count);
+
     }
 
-    static void DFS(int v) {
-        if (visited[v]) return;
-        visited[v] = true;
+    public static void DFS(int i) {
+        if (!visited[i]) {
+            visited[i] = true;
 
-        for (int i: A[v]) {
-            if (!visited[i]) {
-                DFS(i);
+            for (Integer j : arrays[i]) {
+
+                if (visited[j]) {
+                    continue;
+                } else {
+                    DFS(j);
+                }
             }
+
         }
     }
 }
