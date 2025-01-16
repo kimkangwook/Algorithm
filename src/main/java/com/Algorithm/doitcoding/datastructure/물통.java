5f9923126d3edeb4e1d2b84a6ab8856f3b318d8e
kangwook <rlarkddnr1686@naver.com>
2025-01-16 21:20:50 +0900

    물통

---
+++ src/main/java/com/Algorithm/doitcoding/datastructure/물통.java
@@ -0,0 +1,67 @@
+package com.Algorithm.doitcoding.datastructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 물통 {
+    static int[] Sender = {0, 0, 1, 1, 2, 2};
+    static int[] Receiver = {1, 2, 0, 2, 0, 1};
+    static boolean visited[][];
+    static boolean answer[];
+    static int now[];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        now = new int[3];
+        now[0] = scanner.nextInt();
+        now[1] = scanner.nextInt();
+        now[2] = scanner.nextInt();
+        visited = new boolean[201][201];
+        answer = new boolean[201];
+        BFS();
+        for (int i=0;i<201;i++) {
+            if (answer[i]) System.out.print(i+ " ");
+        }
+
+    }
+
+    public static void BFS() {
+        Queue<AB> queue = new LinkedList<>();
+        queue.add(new AB(0, 0));
+        visited[0][0]=true;
+        answer[now[2]] = true;
+        while (!queue.isEmpty()) {
+            AB p = queue.poll();
+            int A = p.A;
+            int B = p.B;
+            int C = now[2] - A - B;
+            for (int k=0;k<6;k++) {
+                int[] next = {A, B, C};
+                next[Receiver[k]] += next[Sender[k]];
+                next[Sender[k]] = 0;
+                if (next[Receiver[k]]>now[Receiver[k]]) {
+                    next[Sender[k]] = next[Receiver[k]] - now[Receiver[k]];
+                    next[Receiver[k]] = now[Receiver[k]];
+                }
+                if (!visited[next[0]][next[1]]) {
+                    visited[next[0]][next[1]] = true;
+                    queue.add(new AB(next[0], next[1]));
+                    if (next[0]==0) {
+                        answer[next[2]] = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+class AB {
+    int A;
+    int B;
+
+    public AB(int a, int b) {
+        A = a;
+        B = b;
+    }
+}
--- src/main/java/com/Algorithm/doitcoding/datastructure/이분그래프.java
+++ src/main/java/com/Algorithm/doitcoding/datastructure/이분그래프.java
@@ -9,57 +9,72 @@ import java.util.StringTokenizer;
 public class 이분그래프 {
 
     static ArrayList<Integer>[] A;
-    static int[] check;
-    static boolean visited[];
-    static boolean isEven;
+    static boolean[] visited;
+    static int[] checked;
+
+    static boolean isBreak = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        for (int t = 0;t<N;t++) {
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<K;i++) {
             st = new StringTokenizer(br.readLine());
+
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
+
             A = new ArrayList[V + 1];
             visited = new boolean[V + 1];
-            check = new int[V + 1];
-            isEven = true;
-            for (int i=1;i<=V;i++) {
-                A[i] = new ArrayList<Integer>();
+            checked = new int[V + 1];
+            isBreak = false;
+            for (int j=1;j<V+1;j++) {
+                A[j] = new ArrayList<>();
             }
-            for (int i=0;i<E;i++) {
+
+            for (int j=0;j<E;j++) {
                 st = new StringTokenizer(br.readLine());
                 int Start = Integer.parseInt(st.nextToken());
                 int End = Integer.parseInt(st.nextToken());
+
                 A[Start].add(End);
                 A[End].add(Start);
             }
 
-            for (int i=1;i<=V;i++) {
-                if (isEven) {
-                    DFS(i);
-                } else
+            for (int s=1;s<=V;s++) {
+                if (isBreak) {
                     break;
+                } else {
+                    DFS(s);
+                }
             }
 
-            if (isEven) {
-                System.out.println("YES");
-            } else {
+            if (isBreak) {
                 System.out.println("NO");
+            } else {
+                System.out.println("YES");
             }
+
         }
     }
 
-    public static void DFS(int node) {
-        visited[node] = true;
-        for (int i : A[node]) {
-            if (!visited[i]) {
-                check[i] = (check[node]+1)%2;
-                DFS(i);
-            } else if (check[node] == check[i]) {
-                isEven = false;
+    public static void DFS(int Node) {
+            visited[Node] = true;
+            for (int next : A[Node]) {
+                if (!visited[next]) {
+                    // 방문 안한 노드일때
+                    checked[next] = (checked[Node]+1) % 2;
+                    DFS(next);
+                } else {
+                    // 방문한 노드일때
+                    int q = (checked[Node] + 1) % 2;
+                    if (q!=checked[next]) {
+                        isBreak = true;
+                    }
+                }
             }
-        }
+
     }
+
 }
