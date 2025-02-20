96f02d4d02036411e91291c21a0232d9f451d53a
kangwook <rlarkddnr1686@naver.com>
2025-02-20 16:01:26 +0900

    구간합

--- src/main/java/com/Algorithm/doitcoding/자료구조/나머지합_10986.java
+++ src/main/java/com/Algorithm/doitcoding/자료구조/나머지합_10986.java
@@ -5,32 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 구간합구하기4_11659 {
+public class 나머지합_10986 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int[] sum = new int[N + 1];
+        long[] sum = new long[N + 1];
+        long[] C = new long[M];
+        long answer = 0;
 
         for (int i=1;i<=N;i++) {
-            int number = Integer.parseInt(st.nextToken());
-            sum[i] = sum[i-1] + number;
+            long number = Long.parseLong(st.nextToken());
+            sum[i] = sum[i - 1] + number;
+        }
+
+        for (int i=1;i<=N;i++) {
+            int remainder = (int)(sum[i] % M);
+            if (remainder==0) {
+                answer++;
+            }
+            C[remainder]++;
         }
 
         for (int i=0;i<M;i++) {
-            st = new StringTokenizer(br.readLine());
-            int startIndex = Integer.parseInt(st.nextToken());
-            int endIndex = Integer.parseInt(st.nextToken());
-            sb.append(sum[endIndex] - sum[startIndex - 1]).append('\n');
+            if (C[i]>1) {
+                answer += (C[i] * (C[i] - 1) / 2);
+            }
         }
-
-        System.out.println(sb);
+        System.out.println(answer);
 
     }
 }
